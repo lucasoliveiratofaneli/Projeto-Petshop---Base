f2b7ff51689cb728cccd857dfa0792722115943f
lucasoliveiratofaneli <lucasoliveirasenaijau@gmail.com>
2026-05-19 14:15:04 -0300

    Site Petshop

--- js/script.js
+++ js/script.js
@@ -1,28 +1,23 @@
-// scripts petshop
+// FORMULÁRIO
+let formulario = document.getElementById("formContato");
 
-document.getElementById("formContato").addEventListener("submit", function (e) {
-  e.preventDefault();
-  let nome = document.getElementById("nome").value.trim();
-  let email = document.getElementById("email").value.trim();
-  let mensagem = document.getElementById("mensagem").value.trim();
+if (formulario) {
+  formulario.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  if (!nome || email == "" || !mensagem) {
-    alert("Preencha todos os campos!");
-    return;
-  }else {
-    alert("Mensagem enviada com sucesso!");
-  }
-  this.reset();
-});
+    let nome = document.getElementById("nome").value.trim();
+    let email = document.getElementById("email").value.trim();
+    let mensagem = document.getElementById("mensagem").value.trim();
 
-// ****************************************** // 
+    if (!nome || email == "" || !mensagem) {
+      alert("Preencha todos os campos!");
+      return;
+    } else {
+      alert("Mensagem enviada com sucesso!");
+    }
 
-let botaoMenu = document.getElementById("menu-btn");
-let menu = document.getElementById("menu");
-
-botaoMenu.addEventListener("click", () => {
-  menu.classList.toggle("ativo");
-});
+    this.reset();
+  });
+}
 
 // ****************************************** //
-
